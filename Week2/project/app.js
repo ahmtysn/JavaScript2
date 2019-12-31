71f193906c8cdf7b01dcba99d95bc8cfc0d23c63
ahmtysn <ahmtysn14@gmail.com>
2019-12-31 16:14:09 +0100

    project developed

--- Week2/project/app.js
+++ Week2/project/app.js
@@ -3,14 +3,14 @@ const upArrow = document.getElementById('up');
 const downArrow = document.getElementById('down');
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
-const timerDisplay = document.querySelector('.timeDisplay');
+const timeDisplay = document.querySelector('.timeDisplay');
 const endTimeDisplay = document.querySelector('.timeEnd');
 const minDisplay = document.getElementById('min');
 const secDisplay = document.getElementById('sec');
 const hourDisplay = document.getElementById('hour');
 const form = document.customForm;
-const audioPlus = document.getElementById('+');
-const audioMinus = document.getElementById('-');
+const audioClick = document.getElementById('click');
+const audioFinish = document.getElementById('finish');
 
 function timer(seconds) {
   clearInterval(countDown);
@@ -20,14 +20,10 @@ function timer(seconds) {
   countDown = setInterval(function() {
     if (seconds > 0) {
       seconds--;
-      const hour = Math.floor(seconds / 60 / 60);
-      const minutes = Math.floor((seconds / 60) % 60);
-      const remainSeconds = seconds % 60;
-      hourDisplay.textContent = `${hour < 10 ? '0' : ''}${hour}`;
-      minDisplay.textContent = `${minutes < 10 ? '0' : ''}${minutes}`;
-      secDisplay.textContent = `${remainSeconds < 10 ? '0' : ''} ${remainSeconds}`;
+      display(seconds);
     } else {
-      timerDisplay.textContent = `Time is Up`;
+      audioFinish.play();
+      timeDisplay.textContent = `Time is Up`;
       clearInterval(countDown);
     }
   }, 1000);
@@ -39,6 +35,14 @@ function endTime(total) {
   const min = end.getMinutes();
   endTimeDisplay.textContent = `End Time ${hrs < 10 ? '0' : ''}${hrs}:${min < 10 ? '0' : ''}${min}`;
 }
+function display(seconds) {
+  const hour = Math.floor(seconds / 60 / 60);
+  const minutes = Math.floor((seconds / 60) % 60);
+  const remainSeconds = seconds % 60;
+  hourDisplay.textContent = `${hour < 10 ? '0' : ''}${hour}`;
+  minDisplay.textContent = `${minutes < 10 ? '0' : ''}${minutes}`;
+  secDisplay.textContent = `${remainSeconds < 10 ? '0' : ''}${remainSeconds}`;
+}
 
 function updateSeconds() {
   const totalSeconds =
@@ -48,44 +52,42 @@ function updateSeconds() {
   return totalSeconds;
 }
 function plus25() {
+  audioClick.play();
   const plus25min = updateSeconds() + 25 * 60;
-  audioPlus.play();
-  timer(plus25min);
+  clearInterval(countDown);
+  display(plus25min);
 }
 function minus25() {
+  audioClick.play();
   const remainMinutes = parseFloat(hourDisplay.innerText * 60) + parseFloat(minDisplay.innerText);
-  audioMinus.play();
   if (remainMinutes >= 25) {
     const min25min = updateSeconds() - 25 * 60;
-    timer(min25min);
+    clearInterval(countDown);
+    display(min25min);
   } else {
     alert("It's impossible! You have less than 25 minutes");
   }
 }
 function stopTimer() {
+  audioClick.play();
   clearInterval(countDown);
-  const seconds = updateSeconds();
-  const hour = Math.floor(seconds / 60 / 60);
-  const minutes = Math.floor((seconds / 60) % 60);
-  const remainSeconds = seconds % 60;
-  hourDisplay.textContent = `${hour < 10 ? '0' : ''}${hour}`;
-  minDisplay.textContent = `${minutes < 10 ? '0' : ''}${minutes}`;
-  secDisplay.textContent = `${remainSeconds < 10 ? '0' : ''} ${remainSeconds}`;
+  display(seconds);
 }
-function resumeTimer() {
+function startTimer() {
+  audioClick.play();
   const seconds = updateSeconds();
   timer(seconds);
 }
 
-// ***************** addEventListener *****************
+// ************ addEventListeners *************
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   const seconds = this.minutes.value * 60;
-  timer(seconds);
+  clearInterval(countDown);
+  display(seconds);
 });
-
 upArrow.addEventListener('click', plus25);
 downArrow.addEventListener('click', minus25);
 stop.addEventListener('click', stopTimer);
-play.addEventListener('click', resumeTimer);
+play.addEventListener('click', startTimer);
