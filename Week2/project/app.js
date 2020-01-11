c10b076c0a73229adc8dec5d002085a09d667962
ahmtysn <ahmtysn14@gmail.com>
2020-01-11 20:31:24 +0100

    update according to feedback

--- Week2/project/app.js
+++ Week2/project/app.js
@@ -1,19 +1,20 @@
-let countDown; // The name of the setInternal that I used
+let countDown; // The id of the setInternal that I used
 
 const form = document.customForm;
-const upArrow = document.getElementById("up");
-const arrangeTime = document.getElementById("arrangeTime");
-const downArrow = document.getElementById("down");
+const upArrow = document.getElementById('up');
+const arrangeTime = document.getElementById('arrangeTime');
+const downArrow = document.getElementById('down');
 
-const minDisplay = document.getElementById("min");
-const secDisplay = document.getElementById("sec");
-const timeDisplay = document.querySelector(".timeDisplay"); // it is for 'Time is Up' phrase
-const endTimeDisplay = document.querySelector(".timeEnd"); // with calculating it will show the finish time
-const play = document.getElementById("play");
-const stop = document.getElementById("stop");
+const endTextDisplay = document.getElementById('end');
+const minDisplay = document.getElementById('min');
+const secDisplay = document.getElementById('sec');
 
-const audioClick = document.getElementById("click");
-const audioFinish = document.getElementById("finish");
+const endTimeDisplay = document.querySelector('.timeEnd'); // with calculating it will show the finish time
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+
+const audioClick = document.getElementById('click');
+const audioFinish = document.getElementById('finish');
 
 function timer(seconds) {
   // It is main function
@@ -27,7 +28,11 @@ function timer(seconds) {
       display(seconds);
     } else {
       audioFinish.play();
-      timeDisplay.textContent = `Time is Up`;
+      endTextDisplay.style.visibility = 'visible';
+      setTimeout(() => {
+        endTextDisplay.style.visibility = 'hidden';
+      }, 3000);
+      arrangeTime.textContent = '00';
       clearInterval(countDown);
     }
   }, 1000);
@@ -38,15 +43,13 @@ function endTime(total) {
   const end = new Date(total); // with milliseconds it will show the real time
   const hrs = end.getHours();
   const min = end.getMinutes();
-  endTimeDisplay.textContent = `End Time ${hrs < 10 ? "0" : ""}${hrs}:${
-    min < 10 ? "0" : ""
-  }${min}`;
+  endTimeDisplay.textContent = `End Time ${hrs < 10 ? '0' : ''}${hrs}:${min < 10 ? '0' : ''}${min}`;
 }
 function display(seconds) {
   const remainMinutes = Math.floor(seconds / 60);
   const remainSeconds = seconds % 60;
-  minDisplay.textContent = `${remainMinutes < 10 ? "0" : ""}${remainMinutes}`;
-  secDisplay.textContent = `${remainSeconds < 10 ? "0" : ""}${remainSeconds}`;
+  minDisplay.textContent = `${remainMinutes < 10 ? '0' : ''}${remainMinutes}`;
+  secDisplay.textContent = `${remainSeconds < 10 ? '0' : ''}${remainSeconds}`;
 }
 
 // updateSeconds => after event listener (pause,play) to protect value of time display
@@ -60,9 +63,10 @@ function plus() {
   let remainMinutes = +arrangeTime.textContent;
   if (remainMinutes < 60) {
     arrangeTime.textContent = remainMinutes + 1; // increase one minute
+    arrangeTime.textContent = `${arrangeTime.textContent < 10 ? 0 : ''}${arrangeTime.textContent}`;
     display(+arrangeTime.textContent * 60);
   } else {
-    alert("You reached the max value. Thank you for your understanding.");
+    alert('You reached the max value. Thank you for your understanding.');
   }
 }
 function minus() {
@@ -71,6 +75,7 @@ function minus() {
   let remainMinutes = +arrangeTime.textContent;
   if (remainMinutes >= 1) {
     arrangeTime.textContent = remainMinutes - 1; // decrease one minute
+    arrangeTime.textContent = `${arrangeTime.textContent < 10 ? 0 : ''}${arrangeTime.textContent}`;
     display(+arrangeTime.textContent * 60);
   } else {
     alert("It's impossible! You are at the zero point!");
@@ -82,14 +87,14 @@ function stopTimer() {
 }
 function startTimer() {
   audioClick.play();
-  form.minutes.value = "";
+  form.minutes.value = '';
   const seconds = updateSeconds();
   timer(seconds);
 }
 
 // ************ addEventListeners *************
 
-form.addEventListener("submit", function(e) {
+form.addEventListener('submit', function(e) {
   e.preventDefault();
   if (this.minutes.value > 0 && this.minutes.value <= 60) {
     const seconds = this.minutes.value * 60;
@@ -100,7 +105,7 @@ form.addEventListener("submit", function(e) {
     alert(`Please give value between 0-60 minutes`);
   }
 });
-upArrow.addEventListener("click", plus);
-downArrow.addEventListener("click", minus);
-stop.addEventListener("click", stopTimer);
-play.addEventListener("click", startTimer);
+upArrow.addEventListener('click', plus);
+downArrow.addEventListener('click', minus);
+stop.addEventListener('click', stopTimer);
+play.addEventListener('click', startTimer);
